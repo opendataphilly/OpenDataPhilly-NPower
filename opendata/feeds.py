35c2c054c0f2a370a3ae7623b44367a159919353
Casey Thomas <cpthomas@gmail.com>
2012-12-28 10:59:46 -0500

    Import comments model. PEP8 formatting

--- opendata/feeds.py
+++ opendata/feeds.py
@@ -2,24 +2,33 @@ from django.contrib.syndication.views import Feed
 from django.utils.feedgenerator import Rss201rev2Feed as Rss2
 from django.shortcuts import get_object_or_404
 from models import Resource, Tag, Idea
+from django.contrib.comments.models import Comment
+
 
 class BaseResourceFeed(Feed):
     feed_type = Rss2
 
     def item_title(self, item):
         return item.name
+
     def item_link(self, item):
         return item.get_absolute_url()
+
     def item_description(self, item):
         return item.short_description
+
     def item_author_name(self, item):
         return item.organization
+
     def item_author_email(self, item):
         return item.contact_email
+
     def item_author_link(self, item):
         return item.contact_url
+
     def item_categories(self, item):
         return item.tags.all()
+
     def item_pubdate(self, item):
         return item.created
 
@@ -33,7 +42,8 @@ class ResourcesFeed(BaseResourceFeed):
 
     def items(self):
         return Resource.objects.order_by('-created')
-    
+
+
 class UpdatesFeed(BaseResourceFeed):
     title = "OpenDataPhilly.org: Resources - Last Updated"
     link = "/feeds/updates/"
@@ -43,7 +53,8 @@ class UpdatesFeed(BaseResourceFeed):
 
     def items(self):
         return Resource.objects.order_by('-last_updated')
-    
+
+
 class IdeasFeed(Feed):
     title = "OpenDataPhilly.org: Ideas"
     link = "/feeds/ideas/"
@@ -53,27 +64,34 @@ class IdeasFeed(Feed):
 
     def items(self):
         return Idea.objects.order_by('-created_by_date')
+
     def item_title(self, item):
         return item.title
+
     def item_link(self, item):
         return item.get_absolute_url()
+
     def item_author_name(self, item):
         return item.author
+
     def item_description(self, item):
         return item.description
 
+
 class TagFeed(BaseResourceFeed):
     description_template = "feeds/resource.html"
-    
+
     def get_object(self, request, tag_id):
         return get_object_or_404(Tag, pk=tag_id)
+
     def title(self, obj):
         return "OpenDataPhilly.org: Resources in %s" % obj.tag_name
+
     def link(self, obj):
         return "/feeds/tag/%i" % obj.id
+
     def description(self, obj):
         return "Resources with the tag %s in the order they were added" % obj.tag_name
 
     def items(self, obj):
         return Resource.objects.filter(tags=obj).order_by('-created')
-   
